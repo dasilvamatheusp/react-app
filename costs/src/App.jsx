f8e53c22f2a0382665f17e72acb2fb7fa09fab69
Matheus Paulino <128553329+dasilvamatheusp@users.noreply.github.com>
2023-07-10 14:14:28 -0300

    Initial commit

---
+++ costs/src/App.jsx
@@ -0,0 +1,31 @@
+import { BrowserRouter as Router, Routes, Route, Link  } from 'react-router-dom';
+import './styles/main.scss';
+import Home from './components/pages/Home';
+import Company from './components/pages/Company';
+import Contact from './components/pages/Contact';
+import NewProject from './components/pages/NewProject';
+import Projects from './components/pages/Projects'
+
+import Container from './components/layout/Container';
+import NavBar from './components/layout/NavBar'
+import Footer from './components/layout/Footer'
+
+function App() {
+  return (
+    <Router>
+      <NavBar/>
+      <Container customClass='min-height'>
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+          <Route exact  path='/projects' element={<Projects />} />
+          <Route exact path='/company' element={<Company />} />
+          <Route exact path='/contact' element={<Contact />} />
+          <Route exact path='/newproject' element={<NewProject />} />
+        </Routes>
+      </Container>
+      <Footer/>
+    </Router>
+  )
+}
+
+export default App
---
+++ costs/src/components/layout/Container.jsx
@@ -0,0 +1,9 @@
+import styles from './Container.module.scss'
+
+const Container = (props) => {
+  return (
+    <div className={`${styles.container} ${styles[props.customClass]}`}>{props.children}
+  </div>)
+}
+
+export default Container;
